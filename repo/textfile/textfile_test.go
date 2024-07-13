98f961298c7d57989d2a73cb841ae5046968c5e6
eymyong <eymmmm123@gmail.com>
2024-07-14 01:57:00 +0700

    fix test

--- repo/textfile/textfile_test.go
+++ repo/textfile/textfile_test.go
@@ -21,30 +21,31 @@ const (
 )
 
 func TestReadDecode_Happy(t *testing.T) {
-	expectedTodo := makeTodos()
-	todoStr := modelToLines(expectedTodo)
+	todosExpected := makeTodos()
+	lines := modelToLines(todosExpected)
 
-	err := os.WriteFile(fileName, []byte(todoStr), 0o664)
+	err := os.WriteFile(fileName, []byte(lines), 0o664)
 	if err != nil {
 		t.Errorf("unexpected err: `%s`", err)
 	}
 
-	todosList, err := readDecode(fileName)
+	todosActual, err := readDecode(fileName)
 	if err != nil {
 		t.Errorf("unexpected err: `%s`", err)
 		return
 	}
 
-	if expectedTodo[0].Id != todosList[0].Id {
-		t.Errorf("expected id: `%s` but got `%s`", expectedTodo[0].Id, todosList[0].Id)
+	if len(todosActual) != len(todosExpected) {
+		t.Errorf("unexpected length '%d', expecting '%d'", len(todosActual), len(todosExpected))
 	}
 
-	if expectedTodo[0].Data != todosList[0].Data {
-		t.Errorf("expected data: `%s` but got `%s`", expectedTodo[0].Data, todosList[0].Data)
-	}
+	for i := range todosActual {
+		actual := todosActual[i]
+		expected := todosExpected[i]
 
-	if string(expectedTodo[0].Status) != string(todosList[0].Status) {
-		t.Errorf("expected status: `%s` but got `%s`", expectedTodo[0].Status, todosList[0].Status)
+		if actual != expected {
+			t.Errorf("unexpected value, expecting='%+v', got='%+v'", expected, actual)
+		}
 	}
 
 	err = os.WriteFile(fileName, []byte{}, 0o664)
@@ -54,32 +55,50 @@ func TestReadDecode_Happy(t *testing.T) {
 	}
 }
 
+func TestReadDecode_err(t *testing.T) {
+	expectederrReadFile := "no data"
+	_, err := readDecode(fileNameErr)
+	if err == nil {
+		t.Errorf("expected err but got nil")
+	}
+
+	// error contain expected error ?
+	if !strings.Contains(err.Error(), expectederrReadFile) {
+		t.Errorf("expected '%s' but got '%s'", expectederrReadFile, err.Error())
+		return
+	}
+}
+
 func TestModelToLine_Happy(t *testing.T) {
-	expectedTodo := model.Todo{
+	todo := model.Todo{
 		Id:     "1",
 		Data:   "one",
 		Status: model.StatusTodo,
 	}
 	// 1: one: TODO
-	todoStr := modelToLine(expectedTodo)
+	todoStr := modelToLine(todo)
 
 	parts := strings.Split(todoStr, ": ")
 
-	if parts[0] != expectedTodo.Id {
-		t.Errorf("expected id: `%s` but got `%s`", expectedTodo.Id, parts[0])
+	if len(parts) != 3 {
+		t.Errorf("unexpected parts, expecting 3 parts, got %d", len(parts))
 	}
 
-	if parts[1] != expectedTodo.Data {
-		t.Errorf("expected data: `%s` but got `%s`", expectedTodo.Data, parts[1])
+	if parts[0] != todo.Id {
+		t.Errorf("expected id: `%s` but got `%s`", todo.Id, parts[0])
 	}
 
-	if parts[2] != string(expectedTodo.Status) {
-		t.Errorf("expected status: `%s` but got `%s`", expectedTodo.Status, parts[2])
+	if parts[1] != todo.Data {
+		t.Errorf("expected data: `%s` but got `%s`", todo.Data, parts[1])
+	}
+
+	if parts[2] != string(todo.Status) {
+		t.Errorf("expected status: `%s` but got `%s`", todo.Status, parts[2])
 	}
 }
 
 func TestModelToLines_Happy(t *testing.T) {
-	expectedTodo := []model.Todo{
+	todos := []model.Todo{
 		{
 			Id:     "1",
 			Data:   "one",
@@ -92,87 +111,68 @@ func TestModelToLines_Happy(t *testing.T) {
 		},
 	}
 
-	// 1: one: TODO
-	// 2: two: DONE
-	todosListStr := modelToLines(expectedTodo)
+	expected := "1: one\n2: two"
+	actual := modelToLines(todos)
 
-	line := strings.Split(todosListStr, "/n")
-	// [{1: one: TODO},{2: two: DONE}]
-	// t.Log(line[0])
-	// t.Log(line[1])
-
-	words := strings.Split(line[0], ": ")
-	if words[0] != expectedTodo[0].Id {
-		t.Errorf("expected id: `%s` but got `%s`", expectedTodo[0].Id, words[0])
+	if actual != expected {
+		t.Errorf("unexpected value: expecting `%s` but got `%s`", expected, actual)
 	}
-
-	if words[1] != expectedTodo[0].Data {
-		t.Errorf("expected data: `%s` but got `%s`", expectedTodo[1].Id, words[1])
-	}
-	//
-	if words[2] != string(expectedTodo[0].Status) {
-		t.Errorf("expected status: `%s` but got `%s`", expectedTodo[2].Id, words[2])
-	}
-
-	// for _, v := range line {
-	// 	words := strings.Split(v, ": ")
-	// 	if words[0] != expectedTodo[0].Id {
-	// 		t.Errorf("expected id: `%s` but got `%s`", words[0], expectedTodo[0].Id)
-	// 	}
-	// 	if words[1] != expectedTodo[0].Data {
-	// 		t.Errorf("expected data: `%s` but got `%s`", words[1], expectedTodo[0].Data)
-	// 	}
-	// 	if words[2] != string(expectedTodo[0].Status) {
-	// 		t.Errorf("expected status: `%s` but got `%s`", words[2], expectedTodo[0].Status)
-	// 	}
-	// }
 }
 
 func TestLineToModel_Happy(t *testing.T) {
-	data := makeTodos()
+	line := "24: foo"
+	expected := model.Todo{
+		Id:   "24",
+		Data: "foo",
+	}
 
-	expectedTodo := modelToLine(data[0])
-
-	parts := strings.Split(expectedTodo, ": ")
-
-	todo, err := lineToModel(expectedTodo)
+	actual, err := lineToModel(line)
 	if err != nil {
 		t.Errorf("unexpected err: `%s`", err)
 		return
 	}
 
-	if parts[0] != todo.Id {
-		t.Errorf("expected id: '%s' but got '%s'", parts[0], todo.Id)
-	}
-
-	if parts[1] != todo.Data {
-		t.Errorf("expected data: '%s' but got '%s'", parts[1], todo.Id)
-	}
-
-	if parts[2] != string(todo.Status) {
-		t.Errorf("expected status: '%s' but got '%s'", parts[2], todo.Id)
+	if actual != expected {
+		t.Errorf("unexpected value, expecting='%+v', got='%+v'", expected, actual)
 	}
 }
 
 func TestLinesToModel_Happy(t *testing.T) {
-	expectedTodo := makeTodos()
-	lines := modelToLines(expectedTodo)
+	lines := "1: one: TODO\n2: two: DONE\n3: three: TODO"
+	expecteds := []model.Todo{
+		{
+			Id:     "1",
+			Data:   "one",
+			Status: model.StatusTodo,
+		},
+		{
+			Id:     "2",
+			Data:   "two",
+			Status: model.StatusDone,
+		},
+		{
+			Id:     "3",
+			Data:   "three",
+			Status: model.StatusTodo,
+		},
+	}
 
-	todos, err := linesToModel(lines)
+	actuals, err := linesToModel(lines)
 	if err != nil {
 		t.Errorf("unexpexted err: `%s`", err)
 		return
 	}
 
-	if expectedTodo[0].Id != todos[0].Id {
-		t.Errorf("expected id: '%s' but got '%s'", expectedTodo[0].Id, todos[0].Id)
+	if len(actuals) != len(expecteds) {
+		t.Errorf("unexpected length '%d', expecting '%d'", len(actuals), len(expecteds))
 	}
 
-	if expectedTodo[0].Data != todos[0].Data {
-		t.Errorf("expected id: '%s' but got '%s'", expectedTodo[0].Data, todos[0].Data)
-	}
+	for i := range expecteds {
+		actual := actuals[i]
+		expected := expecteds[i]
 
-	if string(expectedTodo[0].Status) != string(todos[0].Status) {
-		t.Errorf("expected id: '%s' but got '%s'", expectedTodo[0].Status, todos[0].Status)
+		if actual != expected {
+			t.Errorf("unexpected value, expecting='%+v', got='%+v'", expected, actual)
+		}
 	}
 }
